1712f329be021f809ef563056952aca5bae45ca0
Mohamed Jarboua <mjarboua@e1r5p1.1337.ma>
2024-05-11 18:08:46 +0100

    fixed some problems, and now ned to fix the logo appearing with the button, need to fix  the logic of the menu appearing with  the logo

--- components/navBar/navBar.tsx
+++ components/navBar/navBar.tsx
@@ -1,18 +1,14 @@
 "use client";
 import { ThemeContext } from "@/useContext/context";
-import { useQuery } from "@tanstack/react-query";
 import { useWindowSize } from "@react-hook/window-size";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
-import { useParams, useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { MdOutlineLightMode } from "react-icons/md";
 import { MdOutlineDarkMode } from "react-icons/md";
 import { IoIosMenu } from "react-icons/io";
 import React, { useContext, useEffect, useState } from "react";
 import Image from "next/image";
-import { link } from "fs";
 import { FaXmark } from "react-icons/fa6";
-import Logo from "./logo";
 
 const CheckIfAdmin = async () => {
   return await false;
@@ -29,7 +25,7 @@ const NavBar = () => {
     windowWidth: 0,
     windowHeight: 0,
   });
-  const [menuExpand, setMenuExpand] = useState(true);
+  const [menuExpand, setMenuExpand] = useState(false);
   const router = useRouter();
   const routes: { name: string; path: string; visibility: string }[] = [
     {
@@ -73,9 +69,11 @@ const NavBar = () => {
 
   return (
     <div
-      className={`flex flex-row justify-between items-center sm:w-[50%] sm:h-[18%] h-full sm:px-8 sm:text-sm relative `}
+      className={`flex sm:flex-row flex-col-reverse border-2 border-white sm:justify-between justify-center items-center sm:w-[50%] sm:h-[18%] w-full ${
+        menuExpand ? "h-full" : "h-[18%]"
+      } sm:px-8 sm:text-sm relative `}
     >
-      {!(windowWidth < 640) ? (
+      {!menuExpand ? (
         <div
           onClick={() => HandleClick(router, "/")}
           className="hover:cursor-pointer opacity-50"
@@ -92,8 +90,8 @@ const NavBar = () => {
           />
         </div>
       ) : null}
-      <div className=" flex flex-col items-center">
-        {windowWidth < 640 ? (
+      <div className=" flex sm:flex-col flex-row items-center ">
+        {windowWidth < 640 && (
           !menuExpand ? (
             <FaXmark
               className={`w-[30px] h-[30px]`}
@@ -111,22 +109,23 @@ const NavBar = () => {
               onClick={() => setMenuExpand(true)}
             />
           )
-        ) : null}
+        )}
         <div className="flex flex-col sm:flex-row sm:gap-10 items-center justify-center relative">
-          {routes.map((route, key) => {
-            return route.visibility === "viewer" ? (
-              <div
-                key={key}
-                className={`text-gray-${
-                  theme?.dark ? "300" : "700"
-                } cursor-pointer`}
-                onClick={() => HandleClick(router, route.path)}
-              >
-                {route.name}
-              </div>
-            ) : null;
-          })}
-          <div className="flex justify-center items-center  top-0 right-5">
+          {menuExpand &&
+            routes.map((route, key) => {
+              return route.visibility === "viewer" ? (
+                <div
+                  key={key}
+                  className={`text-gray-${
+                    theme?.dark ? "300" : "700"
+                  } cursor-pointer`}
+                  onClick={() => HandleClick(router, route.path)}
+                >
+                  {route.name}
+                </div>
+              ) : null;
+            })}
+          <div className="flex justify-center items-center">
             {theme?.dark ? (
               <MdOutlineLightMode
                 className={`w-[30px] h-[30px]`}
