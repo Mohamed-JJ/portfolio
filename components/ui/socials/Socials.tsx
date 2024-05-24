1b585554c05186b3581418ec5676973249576228
Mohamed_Jarboua <simojarboue24@gmail.com>
2024-05-24 23:39:25 +0100

    fixed some socials redirections

--- components/ui/socials/Socials.tsx
+++ components/ui/socials/Socials.tsx
@@ -8,27 +8,24 @@ const Socials = ({ theme }: { theme: Theme | null }) => {
   return (
     <div className="w-[40%] flex flex-col pb-5">
       <p
-        className={`${
-          theme?.dark ? "text-gray-300" : "text-gray-700"
-        } font-bold mt-5`}
+        className={`${theme?.dark ? "text-gray-300" : "text-gray-700"
+          } font-bold mt-5`}
       >
         my Socials
       </p>
       <div className="w-full flex flex-row gap-5 mt-5">
-        <FaLinkedin
-          onClick={() =>
-            router.push("https://www.linkedin.com/in/mohamed-jarboua/")
-          }
-          className={`${
-            theme?.dark ? "text-gray-300" : "text-gray-700"
-          } w-[22px] h-[22px] cursor-pointer`}
-        />
-        <FaGithub
-          onClick={() => router.push("https://github.com/mohamed-JJ")}
-          className={`${
-            theme?.dark ? "text-gray-300" : "text-gray-700"
-          } w-[22px] h-[22px] cursor-pointer`}
-        />
+        <a href="https://www.linkedin.com/in/mohamed-jarboua/" target="_blank">
+          <FaLinkedin
+            className={`${theme?.dark ? "text-gray-300" : "text-gray-700"
+              } w-[22px] h-[22px] cursor-pointer`}
+          />
+        </a>
+        <a href="https://github.com/mohamed-JJ" target="_blank">
+          <FaGithub
+            className={`${theme?.dark ? "text-gray-300" : "text-gray-700"
+              } w-[22px] h-[22px] cursor-pointer`}
+          />
+        </a>
       </div>
     </div>
   );
