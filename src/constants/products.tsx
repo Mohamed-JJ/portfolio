6dc8ccfddf11f899fa6af5c7f8765c6f5604710c
Mohamed-JJ <simojarboue24@gmail.com>
2025-10-01 23:23:58 +0100

    [ADD]: added certification to the portfolio

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -166,10 +166,10 @@ export const products = [
 
 export const certifications = [
   {
-    href: "https://aceternity.com",
+    href: "https://coursera.org/share/a55cf2351e3105078cc6596e1e32db01",
     title: "Meta backend developer",
     description:
-      "A design and development studio that focuses on building quality apps.",
+      "Backend and Database driven course with focus on building logical thinking and system design skills.",
     thumbnail: cert1,
     images: [cert1, cert2],
     stack: ["python", "django", "drf"],
@@ -177,29 +177,32 @@ export const certifications = [
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+          I completed the Meta Backend Developer specialization on Coursera,
+          where I gained a solid foundation in backend engineering through
+          practical coding projects and hands-on exercises. The program equipped
+          me with technical expertise in Python programming, Linux commands, Git
+          and version control, SQL, APIs, JSON, XML, and cloud hosting.
+          Alongside mastering these technologies, I developed the ability to
+          build and manage server-side logic, work with databases, and integrate
+          modern backend systems.{" "}
         </p>
         <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
+          Through this training, I built a portfolio of projects that highlight
+          my ability to solve real-world problems using code and backend
+          technologies. I also received structured guidance on preparing for
+          engineering interviews, including both technical and behavioral
+          aspects. This experience has strengthened my ability to design,
+          develop, and deploy scalable backend solutions while giving me the
+          confidence to apply these skills in professional environments.
         </p>{" "}
       </div>
     ),
   },
   {
-    href: "https://algochurn.com",
+    href: "https://www.coursera.org/account/accomplishments/verify/ZBCS5MM9ZYBK?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course",
     title: "Introduction To DevOps",
     description:
-      "Practice for technical interviews with hands on coding challenges.",
+      "Introduction of DevOps methodologies, practices, case studies, and best cases that require DevOps methodologies.",
     thumbnail: cert3,
     images: [cert3, cert4],
     stack: ["Devops", "agile", "CI/CD"],
@@ -207,20 +210,26 @@ export const certifications = [
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+          I completed structured training in DevOps engineering, where I
+          explored the essential characteristics that define modern DevOps
+          practices. The program emphasized the importance of building a culture
+          of shared responsibility, transparency, and learning from failure as
+          key drivers for innovation and collaboration. I gained a strong
+          understanding of Continuous Integration (CI), Continuous Delivery
+          (CD), Infrastructure as Code (IaC), Test Driven Development (TDD), and
+          Behavior Driven Development (BDD), applying these concepts to create
+          more efficient and reliable development workflows.{" "}
         </p>
         <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
+          Throughout the training, I developed hands-on skills in cloud-native
+          microservices, automated continuous deployments, resilient code
+          design, and modern software engineering practices. Beyond the
+          technical aspects, I also learned the organizational impact of
+          DevOps—breaking down silos, working effectively in cross-functional
+          teams, and fostering shared ownership across the software development
+          lifecycle. This combination of technical expertise and cultural
+          principles has strengthened my ability to contribute to
+          high-performing DevOps environments.
         </p>{" "}
       </div>
     ),
